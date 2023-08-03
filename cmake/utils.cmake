9f70d9086105ed1f46e8d2cdbd0bb2441bd66335
浮生若梦 <1070753498@qq.com>
2023-08-03 18:36:38 +0800

    unix;

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,10 +1,15 @@
 function(add_custom_library target_name)
-  add_library(${target_name} SHARED ${ARGN})
-  add_custom_command(
-    TARGET ${target_name}
-    POST_BUILD
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${target_name}>
-            ${EXECUTABLE_OUTPUT_PATH}/$<TARGET_FILE_NAME:${target_name}>)
+  if(CMAKE_HOST_WIN32)
+    add_library(${target_name} SHARED ${ARGN})
+    add_custom_command(
+      TARGET ${target_name}
+      POST_BUILD
+      COMMAND
+        ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${target_name}>
+        ${EXECUTABLE_OUTPUT_PATH}/$<TARGET_FILE_NAME:${target_name}>)
+  else()
+    add_library(${target_name} ${ARGN})
+  endif()
 endfunction(add_custom_library)
 
 function(add_custom_plugin target_name)
@@ -16,3 +21,12 @@ function(add_custom_plugin target_name)
             ${EXECUTABLE_OUTPUT_PATH}/plugins/$<TARGET_FILE_NAME:${target_name}>
   )
 endfunction(add_custom_plugin)
+
+function(add_share_library target_name)
+  add_library(${target_name} SHARED ${ARGN})
+  add_custom_command(
+    TARGET ${target_name}
+    POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${target_name}>
+            ${EXECUTABLE_OUTPUT_PATH}/$<TARGET_FILE_NAME:${target_name}>)
+endfunction(add_share_library)
